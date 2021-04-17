b4da9ce2219a63797a3b91de8ee1022e570d6825
Bruno Braga <bruno.braga@fcagroup.com>
2021-04-17 16:48:32 -0300

    first implementation

--- custom_components/apsystems/sensor.py
+++ custom_components/apsystems/sensor.py
@@ -5,40 +5,55 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.helpers.sun import get_astral_event_date
 from homeassistant.util.dt import utcnow as dt_utcnow, as_local
 import voluptuous as vol
-from datetime import timedelta
+import pprint
+import requests
+from requests.adapters import HTTPAdapter
+from datetime import datetime, timedelta, date
 
-CONF_IP_ADDRESS = 'ip_address'
+CONF_USERNAME = 'username'
+CONF_PASSWORD = 'password'
+CONF_ECU_ID = 'ecuId'
+CONF_SYSTEM_ID = 'systemId'
 CONF_NAME = 'name'
 POWER_UNIT = "kWh"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_ECU_ID): cv.string,
+    vol.Required(CONF_SYSTEM_ID): cv.string,
     vol.Optional(CONF_NAME, default='APsystems'): cv.string
 })
 from homeassistant.const import (
     CONF_MONITORED_CONDITIONS, CONF_NAME, CONF_SCAN_INTERVAL, ATTR_ATTRIBUTION, SUN_EVENT_SUNRISE, SUN_EVENT_SUNSET
     )
 
-DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
+SCAN_INTERVAL = timedelta(seconds=300)
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    name = config.get(CONF_NAME)
-    ip_address = config[CONF_IP_ADDRESS]
+    sensor_name = config.get(CONF_NAME)
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+    ecu_id = config[CONF_ECU_ID]
+    system_id = config[CONF_SYSTEM_ID]
 
-    sensor = ApsystemsSensor(name, ip_address)
+    sensor = ApsystemsSensor(sensor_name, username, password, ecu_id, system_id)
     async_add_entities([sensor])
 
 
 class ApsystemsSensor(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self, name, ip_address):
+    def __init__(self, sensor_name, username, password, ecu_id, system_id):
         """Initialize the sensor."""
         self._state = None
-        self._name = name
-        self._ip_address = ip_address
+        self._name = sensor_name
+        self._username = username
+        self._password = password
+        self._ecu_id = ecu_id
+        self._system_id = system_id
 
     @property
     def name(self):
@@ -72,6 +87,10 @@ class ApsystemsSensor(Entity):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
+        fetcher = APsystemsFetcher(self._username, self._password, self._ecu_id, self._system_id)
+        data = fetcher.data()
+        _LOGGER.debug(pprint.pformat(data))
+
         self._state = 23
 
     def find_start_time(self, now):
@@ -83,3 +102,42 @@ class ApsystemsSensor(Entity):
         """Return sunset or stop_time if given."""
         sunset = get_astral_event_date(self.hass, SUN_EVENT_SUNSET, now.date())
         return sunset
+
+
+class APsystemsFetcher:
+    url_login = "https://apsystemsema.com/ema/loginEMA.action"
+    #url_profile = "https://ema.api.apsystemsema.com:9223/aps-api-web/api/view/registration/user/getUserInfoWithSystemInfo"
+    url_info = "https://apsystemsema.com/ema/ajax/getDownLoadReportApiAjax/getHourlyEnergyOnCurrentDayAjax"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0'}
+
+    def __init__(self, username, password, ecu_id, system_id):
+        self._username = username
+        self._password = password
+        self._ecu_id = ecu_id
+        self._system_id = system_id
+        self.today = datetime.fromisoformat(date.today().isoformat())
+
+    def login(self):
+        params = {'today': datetime.today().strftime("%Y-%m-%d+%H:%M:%S"),
+                  'username':	self._username,
+                  'password':	self._password}
+
+        session = requests.session()
+        session.mount('https://', HTTPAdapter())
+
+        # should be call twice to correctly display
+        session.request("POST", self.url_login, data=params, headers=self.headers)
+
+        return session
+
+    def data(self):
+        session = self.login()
+
+        params = {'queryDate': datetime.today().strftime("%Y%m%d"),
+                  'ecuId':  self._ecu_id,
+                  'userId':  self._system_id,
+                  'systemId': self._system_id}
+
+        result_data = session.request("POST", self.url_info, data=params, headers=self.headers)
+
+        return result_data.json()
